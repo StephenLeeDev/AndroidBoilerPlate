f2ad08df32e61876a161da6349d86e2810f4d152
StephenLeeDev <stephen.lee.dev@gmail.com>
2022-07-25 16:25:21 +0900

    Add | Generate SharedPreferences Class

---
+++ app/src/main/java/com/stephen/androidbolierplate/data/util/PrefUtil.kt
@@ -0,0 +1,27 @@
+package com.stephen.androidbolierplate.data.util
+
+import android.content.Context
+import android.content.SharedPreferences
+
+/**
+ * Written by StephenLeeDev on 2022/07/25.
+ */
+
+class PrefUtil(context : Context) {
+
+    private val sharedPreferences: SharedPreferences by lazy {
+        context.getSharedPreferences(
+            "boiler-plate-pref",
+            Context.MODE_PRIVATE
+        )
+    }
+
+    fun setUserAccessToken(token: String) {
+        sharedPreferences.edit().putString("user_access_token", token).apply()
+    }
+
+    fun getUserAccessToken(): String {
+        return sharedPreferences.getString("user_access_token", "") ?: ""
+    }
+
+}
