4a9e891ee5092bd91f7360092479d2732fe5beb3
StephenLeeDev <stephen.lee.dev@gmail.com>
2022-08-02 18:16:30 +0900

    Delete | Delete Unnecessary Files & Source Code

--- app/src/main/java/com/stephen/androidbolierplate/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/stephen/androidbolierplate/presentation/ui/main/MainActivity.kt
@@ -2,40 +2,26 @@ package com.stephen.androidbolierplate.presentation.ui.main
 
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.fragment.app.viewModels
 import com.stephen.androidbolierplate.R
 import com.stephen.androidbolierplate.databinding.ActivityMainBinding
 import com.stephen.androidbolierplate.presentation.ui.base.BaseActivity
-import com.stephen.androidbolierplate.presentation.ui.main.chat.ChatRoomAdapter
-import com.stephen.androidbolierplate.presentation.ui.main.chat.ChatRoomViewModel
 import com.stephen.androidbolierplate.presentation.util.setupWithNavController
 
 class MainActivity : BaseActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val userViewModel: UserViewModel by viewModels()
-//    private val chatRoomViewModel: ChatRoomViewModel by viewModels()
-
-    private val adapter = ChatRoomAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         setUpBottomNavigation()
-        initViews()
-        initObservers()
         getUserInfo()
     }
 
     private fun getUserInfo() {
         userViewModel.getUserInfo()
-    }
-
-    private fun initViews() {
-        binding.apply {
-            recyclerView.adapter = adapter
-        }
     }
 
     private fun setUpBottomNavigation() {
@@ -55,19 +41,4 @@ class MainActivity : BaseActivity() {
         )
     }
 
-    private fun initObservers() {
-//        boilerPlateViewModel.boilerPlateState.observe(this@MainActivity) { state ->
-//            when (state) {
-//                is BoilerPlateState.Progress -> {
-//                    // TODO : 통신 중
-//                }
-//                is BoilerPlateState.Success -> {
-//                    adapter.submitList(state.list)
-//                }
-//                is BoilerPlateState.Fail -> {
-//                    // TODO : 통신 실패
-//                }
-//            }
-//        }
-    }
 }
